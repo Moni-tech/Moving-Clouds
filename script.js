765e11d30519bafae80eb318ad90873e0cf45a82
Monisha <monishaaradhyacm@gmail.com>
2025-10-22 18:42:01 +0530

    Initial commit: add project files

---
+++ script.js
@@ -0,0 +1,7 @@
+const elements = document.getElementsByClassName("cloud");
+const element = elements[0];
+element.style.top = "70px";
+element.style.left = "50px";
+element.style.top = Math.random();
+element.style.left = Math.random();
+element.style.transform = "scale(0.3)";
